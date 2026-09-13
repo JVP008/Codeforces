7529e0f21d69879d22521f1eef943cd549136a9f
JVP008 <jayeshpatil20081234@gmail.com>
2026-09-13 19:38:58 +0000

    :(

--- 800/617A.cpp
+++ 800/617A.cpp
@@ -1,7 +1,12 @@
-
 #include<bits/stdc++.h>
 using namespace std;
-int main (){
+typedef long long l;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
+    l n ;
+    cin>>n;
+    cout<<(n+4)/5;
     return 0;
 }
