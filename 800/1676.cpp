d91af609507086a1d45247aca2a87762ca4f39b1
JVP008 <jayeshpatil20081234@gmail.com>
2026-09-16 19:22:55 +0000

    :)

--- 800/1676.cpp
+++ 800/1676.cpp
@@ -1,19 +1,31 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int A,B;
-    cin>>A>>B;
-    int count = 0;
-    if ((A+B)%2==1){
-        cout<<-1;
-    }
-    else{
-        while(A!=B && A>B){
-        A-=B;
-        count+=1;
-    }
-    cout<<count<<"\n";
+int main (){
+    int T;
+    cin>>T;
+    while(T--){
+        string s;
+        cin>>s;
+        int size = s.size();
+        vector<int>left ;
+        vector<int>right ;
+        int totall =0;
+        int totalr =0;
+        for (int i =0;i<3;i++){
+            int t = s[i]-'0';
+            totall+=t;
+        }
+        int half = size/2;
+        for (int i =3;i<6;i++){
+            int t = s[i]-'0';
+            totalr+=t;
+        }
+        if (totall==totalr){
+            cout<<"YES\n";
+        }
+        else{
+            cout<<"NO\n";
+        }
     }
     return 0;
 }
